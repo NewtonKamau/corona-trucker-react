ea2507f51819ddfe8721c8daa3269a565e7155dc
Newton <kamaunewton78@gmail.com>
2020-09-04 12:15:25 +0300

    useEffect to get data to the app.js and [] to manage the loop

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {  useEffect } from "react";
 import { Cards, Chart, CountryPicker } from "./components/";
 import styles from "./App.module.css";
-
+import { fetchData } from "./apis";
 function App() {
+    useEffect(() => {
+       async function getData ()  {
+        const data = fetchData()
+        console.log(data);
+      };
+   getData()
+    }, []);
   return (
     <div className={styles.container}>
       <Cards />
