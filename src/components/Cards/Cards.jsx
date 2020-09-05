3cddc7f7321741f2abc16ff8e9074db8c01c1867
Newton <kamaunewton78@gmail.com>
2020-09-05 12:18:48 +0300

    added count up

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Card,CardContent,Typography,Grid, StylesProvider} from "@material-ui/core";
+import { Card, CardContent, Typography, Grid, StylesProvider } from "@material-ui/core";
+import CountUp from "react-countup";
 import styles from "./Cards.module.css";
 
 
@@ -15,7 +16,15 @@ const Cards = ({ data: { confirmed, recoveries, deaths } }) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.75}
+                separator=","
+
+              />
+            </Typography>
             <Typography color="textSecondary">Date</Typography>
             <Typography variant="body2">
               Number of Active Cases of Covid-19
