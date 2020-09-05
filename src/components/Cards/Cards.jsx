341f80327199fff3caab23244598d55ebf97cd07
Newton <kamaunewton78@gmail.com>
2020-09-05 12:03:41 +0300

    receive the data to the ui from props

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import {Card,CardContent,Typography,Grid, StylesProvider} from "@material-ui/core";
+import styles from "./Cards.module.css";
 
-const Card = (props) => {
-    console.log(props);
-    return (
-        <h1>Cards</h1>
-    );
-}
 
-export default Card;
+const Cards = ({ data: { confirmed, recoveries, deaths } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography color="textSecondary">Date</Typography>
+            <Typography variant="body2">
+              Number of Active Cases of Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recoveries
+            </Typography>
+            <Typography variant="h5">Real data</Typography>
+            <Typography color="textSecondary">Date</Typography>
+            <Typography variant="body2">
+              Number of recovered Cases of Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">Real data</Typography>
+            <Typography color="textSecondary">Date</Typography>
+            <Typography variant="body2">Death Cases of Covid-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Cards;
