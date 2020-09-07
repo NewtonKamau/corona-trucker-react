6399925f90ff85106c95da7d62593f76a495357a
Newton <kamaunewton78@gmail.com>
2020-09-07 12:00:36 +0300

    fetch  data to the ui

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../apis";
+import { Line, Bar } from "react-chartjs-2";
+import { styles } from "./Chart.module.css";
 
 const Chart = () => {
-  return <h1>Charts</h1>;
+  //use state to read current data and update the data 
+  const [dailyData, setDailyData] = useState([]);
+  //useEffect to read data from the response of our api and set the state with the current data 
+  useEffect(() => {
+     const fetchedData = async () => {
+       setDailyData(await fetchDailyData());
+     };
+    //call fetch method
+     fetchedData();
+   }, []);
+//show our data in a line chart from chartjs passing the data from our setDailyData
+  const lineChart = (
+    dailyData.length ? (
+    <Line
+        data={{
+        //map data to the array
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#333fff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor:'rgba(255,0,0,0.5)',
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null);
+  return (
+    <div>
+      {lineChart}
+    </div>
+  );
+  
 };
 
 export default Chart;
