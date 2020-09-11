68d951b1c05e001ae0c8138f0d985c3b81578707
Newton <kamaunewton78@gmail.com>
2020-09-11 11:44:18 +0300

    fetch country from our api and setState for it then show single coutry to the country picker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { NativeSelect,FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
+import { countryData }  from "../../apis";
 const CountryPicker = () => {
-  return <h1>CountryPicker</h1>;
+  const [fetchedCountry, setFetchedCountry] = useState([]);
+  useEffect(() => {
+   const fetchCountry = async () => {
+     setFetchedCountry(await countryData());
+   };
+   //call fetch method
+   fetchCountry();
+  }, [setFetchedCountry]);
+ 
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">GLobal</option>
+        {fetchedCountry.map((country,i) => <option key={i} value={country}>{country}</option>)}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default CountryPicker;
