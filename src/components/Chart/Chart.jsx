d5f43405cd5590821f6b745324da2ee4676466ef
NewtonKamau <kamaunewton78@gmail.com>
2020-10-06 13:46:10 +0300

    change line  and bar chart if the country is selected and added some css and also responsiveness

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,45 +1,72 @@
-import React, { useState, useEffect } from "react";
-import { fetchDailyData } from "../../apis";
-import { Line, Bar } from "react-chartjs-2";
-import  styles  from "./Chart.module.css";
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
 
-const Chart = () => {
-  //use state to read current data and update the data 
-  const [dailyData, setDailyData] = useState([]);
-  //useEffect to read data from the response of our api and set the state with the current data 
+import { fetchDailyData } from '../../apis';
+
+import styles from './Chart.module.css';
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState({});
+
   useEffect(() => {
-     const fetchedData = async () => {
-       setDailyData(await fetchDailyData());
-     };
-    //call fetch method
-     fetchedData();
-   }, []);
-//show our data in a line chart from chartjs passing the data from our setDailyData
-   const lineChart = dailyData.length ? (
-     <Line
-       data={{
-         labels: dailyData.map(({ date }) => date),
-         datasets: [
-           {
-             data: dailyData.map((data) => data.confirmed),
-             label: "Infected",
-             borderColor: "#3333ff",
-             fill: true,
-           },
-           {
-             data: dailyData.map((data) => data.deaths),
-             label: "Deaths",
-             borderColor: "red",
-             backgroundColor: "rgba(255, 0, 0, 0.5)",
-             fill: true,
-           },
-         ],
-       }}
-     />
-   ) : null;
-  return <div className={styles.container}>
-    {lineChart}</div>;
-  
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
+    };
+
+    fetchMyAPI();
+  }, []);
+
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
+
+  const lineChart = (
+    dailyData[0] ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [{
+            data: dailyData.map((data) => data.confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          },
+          ],
+        }}
+      />
+    ) : null
+  );
+
+  return (
+    <div className={styles.container}>
+      {country ? barChart : lineChart}
+    </div>
+  );
 };
 
 export default Chart;
