750a50d6847c461b44950b5f35c292c014f07e00
NewtonKamau <kamaunewton78@gmail.com>
2020-10-06 14:03:48 +0300

    Refactored all the components and made them responsive

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { NativeSelect,FormControl } from "@material-ui/core";
-import styles from "./CountryPicker.module.css";
-import { countryData }  from "../../apis";
-const CountryPicker = ({handleCountryChange}) => {
-  const [fetchedCountry, setFetchedCountry] = useState([]);
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import {countryData } from '../../apis';
+
+import styles from './CountryPicker.module.css';
+
+const CountryPicker = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
+
   useEffect(() => {
-   const fetchCountry = async () => {
-     setFetchedCountry(await countryData());
-   };
-   //call fetch method
-   fetchCountry();
+    const fetchAPI = async () => {
+      setCountries(await countryData());
+    };
+
+    fetchAPI();
   }, []);
- 
+
   return (
     <FormControl className={styles.formControl}>
-      <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
-        <option value="global">Global</option>
-        {fetchedCountry.map((country, i) => (
-          <option key={i} value={country}>
-            {country}
-          </option>
-        ))}
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value="">Global</option>
+        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
       </NativeSelect>
     </FormControl>
   );
